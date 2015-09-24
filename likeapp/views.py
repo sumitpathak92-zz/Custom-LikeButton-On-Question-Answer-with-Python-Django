ca1bfb23ea24bb85290fa0a36a7380651f8fe280
sumitpathak92 <pathaksumit92@gmail.com>
2015-09-24 13:25:46 +0530

    changes

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404 
-
-from django.template import Template, Context 
 import datetime
-from django.template.loader import get_template
 # Create your views here.
 
 def hello(request):#request object contains info about the current web request that triggered this view
@@ -20,15 +17,9 @@ def homepage_view(request):
 
 def server_time(request):
     timenow = datetime.datetime.now()
-    t = get_template('current_datetime.html')
-    html = t.render(Context({'current_time' : timenow}))
-    return HttpResponse(html)
+    return render(request, 'current_datetime.html', {'current_time': timenow})
 
 def hours_ahead(request, offset):
-    try:
-        offset = int(offset)
-    except ValueError:
-        raise Http404()
+    offset = int(offset)
     dt = datetime.datetime.now() + datetime.timedelta(hours = offset)
-    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-    return HttpResponse(html)
+    return render(request, 'hours_ahead.html', {'hour_offset' : offset, 'next_time' : dt})
